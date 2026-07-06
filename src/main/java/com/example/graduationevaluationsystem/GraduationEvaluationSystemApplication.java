f64967bd6b2a17dd99b6597a7c1c11c8fe085c33
raoyc <raoyc@asiainfo.com>
2026-07-06 10:04:49 +0800

    初始化仓库

---
+++ src/main/java/com/example/graduationevaluationsystem/GraduationEvaluationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduationevaluationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationEvaluationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationEvaluationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graduationevaluationsystem/GraduationEvaluationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduationevaluationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationEvaluationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
